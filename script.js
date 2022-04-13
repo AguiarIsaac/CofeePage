496a8c824857d39fc51b65d058af53d98bb3b4cd
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2022-04-13 16:30:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// aqui pego os componentes da DOM que vou fazer alterações
+const btn = document.getElementById('btn')
+const menu = document.getElementById('menu')
+const iconMenu = document.getElementById('icon_menu')
+
+function abreFechaMenu() {
+	menu.classList.toggle('active') // o .toggle adiciona ou remove a classe que eu desejo
+	if (menu.classList.value === 'active') { // . value me traz ezatamente a class
+		icon_menu.setAttribute('src', 'assets/menu-buguer-close.svg');
+	} else {
+		icon_menu.setAttribute('src', 'assets/menu-buguer-open.svg');
+	}
+}
+
+btn.addEventListener('click', abreFechaMenu)
